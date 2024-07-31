021bd5680fe7064f6e14e1b6bb79242631b738c1
juseipel43 <juseipel43@gmail.com>
2024-07-31 14:08:22 -0400

    Update basic fetch.js

--- basic fetch.js
+++ basic fetch.js
@@ -4,7 +4,7 @@ export function getData() {
   const url = 'https://juseipel43.github.io/TTRPG';
 fetch(url);
   .then((resp)=>{
-  console.log(resp
+  console.log(resp);
   });
   .then()
   .catch();
