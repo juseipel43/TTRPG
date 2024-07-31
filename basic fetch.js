300bb2845fb8ebcd50879ced0d17fb0a5702c718
juseipel43 <juseipel43@gmail.com>
2024-07-31 14:04:49 -0400

    Update basic fetch.js

--- basic fetch.js
+++ basic fetch.js
@@ -3,5 +3,10 @@
 export function getData() {
   const url = 'https://juseipel43.github.io/TTRPG/test.JSON';
 fetch(url);
-  
+  .then((resp)=>{
+  console.log(resp
+  });
+  .then()
+  .catch();
 }
+
